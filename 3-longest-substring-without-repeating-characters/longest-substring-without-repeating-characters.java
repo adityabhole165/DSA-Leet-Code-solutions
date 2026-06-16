bb1073f3b96ed1fe830c281cb0690ed6e1b63ffb
Aditya Bhole <130380292+adityabhole165@users.noreply.github.com>
2026-06-16 10:45:01 +0530

    Time: 5 ms (86.29%) | Memory: 45.5 MB (73.14%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,22 +1,22 @@
 import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //Optimized Sliding window with HashSet 
-        // Time Complexity  - O(n)-each char added/removed at most once
-        //Space Complexity - O(k) - uniquw chars in current window 
-        Set<Character> window = new HashSet<>();
-        int left = 0 , max = 0 ;
-        for(int right = 0; right < s.length(); right++) {
-            //shrink window from left until no duplicate exists
-            while(window.contains(s.charAt(right)))  {
-                window.remove(s.charAt(left));
-                left++;
-            }
+      // HashMap 
+      // stores last seen index -> left pointer jumps directly
+      // Time Complexity - O(n) | Space Complexity - O(k)
+       Map<Character,Integer> map = new HashMap<>();
+       int left = 0, max = 0;
 
-            window.add(s.charAt(right));
-            max = Math.max(max,  right - left + 1);
-        }
-        return max;
+       for(int right = 0; right < s.length(); right++){
+            char c = s.charAt(right);
+            // if char was seen and its last position is inside current window
+            if(map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1; //jump left directly past the duplicate 
+            }
+            map.put(c, right); //update last seen index 
+            max = Math.max(max, right - left + 1); // update max window size 
+       }
+       return max;
     }
 }
