575ad81e42cd48938dee35a4e635bf3142934976
Aditya Bhole <130380292+adityabhole165@users.noreply.github.com>
2026-06-19 10:08:37 +0530

    Time: 5 ms (54.20%) | Memory: 46.8 MB (37.13%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -4,40 +4,27 @@ class Solution {
             return s;
         }
 
-        int n = s.length();
-        int cycleLen = 2 * numRows - 2;
-        int numCols = (n /cycleLen + 1)* (numRows + 1);
-        char[][] grid =  new char[numRows][numCols];
-
-        int row = 0,col = 0;
-        int index= 0;
-
-        while (index < n) {
-            // go down 
-            while(row < numRows && index < n){
-                grid[row][col] = s.charAt(index++);
-                row++;
-            }
-            row -= 2;
-            col++;
-
-            //do diagonally  up 
-            while(row > 0 && col < numCols && index < n) {
-                grid[row][col] = s.charAt(index++);
-                row--;
-                col++;
-            }
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for(int  i = 0 ; i< numRows; i++){
+            rows[i] = new StringBuilder();
         }
 
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for(char c : s.toCharArray()){
+            rows[currentRow].append(c);
+
+            if(currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1:-1;
+        }
         StringBuilder result = new StringBuilder();
-        for(char[] r: grid) {
-            for(char c : r) {
-                if(c!= '\u0000'){
-                    result.append(c);
-                }
-            }
+        for(StringBuilder row : rows){
+            result.append(row);
         }
-
         return result.toString();
     }
 }
